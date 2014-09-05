6ba8e987ace2ece5e4a9634b801ed9b059b92895
Daniel Mesa <danielromanmesa@gmail.com>
2014-09-05 09:02:17 -0400

    Changed to a ModelandView way of serving the data.

--- BallparkDatabase/src/com/nothingtothetable/ballparkdatabase/controller/BallparkDatabaseController.java
+++ BallparkDatabase/src/com/nothingtothetable/ballparkdatabase/controller/BallparkDatabaseController.java
@@ -3,20 +3,18 @@ package com.nothingtothetable.ballparkdatabase.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/ballparkdatabase")
 public class BallparkDatabaseController {
 	String message;
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String getRoot() {
-		return "ballparkdatabase";
+	public ModelAndView getToolRoot() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("ballparkdatabase");
+		return mav;
 	}
 }
 
-/*
-@RequestMapping(method=RequestMethod.GET)
-	public String getToolRoot() {
-		return "mfr/mfrTool";
-	}
-*/

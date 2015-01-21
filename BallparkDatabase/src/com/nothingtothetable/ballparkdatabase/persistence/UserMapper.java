482684858c2f9918908acdfa1a387d2b1d0b18d0
Daniel Mesa <danielromanmesa@gmail.com>
2015-01-21 08:54:00 -0500

    Removing ballparkdatabase stuff
    Why was that in there in the first place?!

--- BallparkDatabase/src/com/nothingtothetable/ballparkdatabase/persistence/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.nothingtothetable.ballparkdatabase.persistence;
-
-import java.util.List;
-
-import com.nothingtothetable.ballparkdatabase.domain.User;
-
-public interface UserMapper {
-	public List<User> getAllUsers();
-}
--- BallparkDatabase/war/WEB-INF/jsp/ballparkdatabase.jsp
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-	<h1>BallparkDatabase Example</h1>
- 
-	<h3>test123</h3>	
-</body>
-</html>
